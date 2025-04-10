a731203e6590da4c64cc9f2e01f8e8b5d742d1e1
AhmedMohamed5393 <mody539325@gmail.com>
2025-04-10 15:29:38 +0200

    fix unauthorized issue while connecting with neon database server

--- db/database.config.ts
+++ db/database.config.ts
@@ -10,6 +10,7 @@ export const dataSourceOptions: DataSourceOptions = {
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  ssl: { rejectUnauthorized: false },
   entities: ['dist/../**/*.entity.js'],
   synchronize: true,
   subscribers: [],
